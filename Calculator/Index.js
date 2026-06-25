7ec821964e1d50ffd73441b86aecc5e5a0c9013a
Maxim Novak <243660992+Maximusprime20@users.noreply.github.com>
2026-06-25 20:46:04 +0200

    Add files via upload

---
+++ Calculator/Index.js
@@ -0,0 +1,24 @@
+// calculator first test:
+
+const display = document.getElementById("display");
+
+
+
+function icons(input){
+    display.value += input;
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+    
+}
+
+
+function clearDisplay(){
+    display.value = "";
+}
